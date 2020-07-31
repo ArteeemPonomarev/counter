4b6257d89e6ae561860ff1fe8d12041c5d4e54e6
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-07-31 11:23:16 +0300

    Updates

---
+++ precache-manifest.6a34a580796b2336a258f50427f28411.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516e8b30ae5b4dafd9d1949415e16511",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "2d960490233b5d8a0b61",
+    "url": "/counter/static/css/main.683c2525.chunk.css"
+  },
+  {
+    "revision": "9735fba2c541e26dee2f",
+    "url": "/counter/static/js/2.0823e040.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counter/static/js/2.0823e040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d960490233b5d8a0b61",
+    "url": "/counter/static/js/main.438862b2.chunk.js"
+  },
+  {
+    "revision": "ce2028c4d969eb4cca45",
+    "url": "/counter/static/js/runtime-main.f27a4559.js"
+  }
+]);
